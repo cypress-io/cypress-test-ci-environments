3cd719c0d9f618879a33b54ca14cbe5453812f43
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-17 11:46:00 -0400

    test how Cypress verify and run retry automatically

--- README.md
+++ README.md
@@ -7,3 +7,21 @@
 Uses different Docker images to install Cypress and runs a test to verify
 that a good error message is shown if the environment is missing XFVB
 or a dependency.
+
+## Running in Docker
+
+To start a docker container
+
+```shell
+docker run -it -v $PWD:/e2e -w /e2e cypress/base:8 /bin/bash
+```
+
+Then install Cypress and run tests
+
+## Debugging
+
+Run tests with environment variable `DEBUG` for example
+
+```
+DEBUG=test npm run test-display-retry
+```
---
+++ test/display-retry-spec.js
@@ -0,0 +1,57 @@
+const execa = require('execa')
+const expect = require('chai').expect
+const debug = require('debug')('test')
+
+describe('environment with invalid DISPLAY', () => {
+  it('retries cypress verify', () => {
+    return execa
+      .shell('$(npm bin)/cypress verify', {
+        env: {
+          DISPLAY: 'wrong-display-value'
+        }
+      })
+      .then(({ stdout, code, stderr }) => {
+        debug('exit code', code)
+        debug('stderr', stderr)
+        debug('stdout', stdout)
+
+        expect(code, 'finished with 0').to.equal(0)
+        expect(stderr, 'no error output').to.equal('')
+        expect(stdout, 'includes warning').to.include(
+          'Warning: Cypress failed to start'
+        )
+        expect(stdout, 'includes incorrect display').to.include(
+          'wrong-display-value'
+        )
+        expect(stdout, 'includes verified message').to.include(
+          'Verified Cypress!'
+        )
+      })
+  })
+
+  it.only('retries cypress run', () => {
+    return execa
+      .shell('$(npm bin)/cypress run', {
+        env: {
+          DISPLAY: 'wrong-display-value'
+        }
+      })
+      .then(({ stdout, code, stderr }) => {
+        debug('exit code', code)
+        debug('stderr', stderr)
+        debug('stdout', stdout)
+
+        expect(code, 'finished with 0').to.equal(0)
+        expect(stderr, 'no error output').to.equal('')
+        expect(stdout, 'includes warning').to.include(
+          'Warning: Cypress failed to start'
+        )
+        expect(stdout, 'includes incorrect display').to.include(
+          'wrong-display-value'
+        )
+        expect(stdout, 'includes verified message').to.include(
+          'Cypress will attempt to fix the problem and rerun'
+        )
+      })
+  })
+})
